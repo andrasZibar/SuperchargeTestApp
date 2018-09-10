3fd8c23d7597d747107084de9f3233cd8f104151
Andras Zibar <zibandris@gmail.com>
2018-09-10 15:42:48 +0200

    activity to display movie json

---
+++ app/src/main/java/com/codecool/zibi/superchargetestapp/activity/SearchMovieDBActivity.java
@@ -0,0 +1,69 @@
+package com.codecool.zibi.superchargetestapp.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.TextView;
+
+import com.codecool.zibi.superchargetestapp.R;
+import com.codecool.zibi.superchargetestapp.service.JSONReader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchMovieDBActivity extends AppCompatActivity {
+    public static final String APIKey = "43a7ea280d085bd0376e108680615c7f";
+    JSONReader jsonReader = new JSONReader();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_movie_db);
+
+        Intent intent = getIntent();
+        String searchQuery = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        String searchURL = "https://api.themoviedb.org/3/search/movie?api_key="
+                + APIKey
+                + "&language=en-US&query="
+                + searchQuery
+                + "&page=1&include_adult=false";
+
+        try {
+            JSONObject[] searchResults = (JSONObject[]) jsonReader.getJson(searchURL).get("results");
+            List<Integer> allMovieIdsFromSearch = new ArrayList<>();
+
+            for (int i = 0; i < searchResults.length; i++) {
+                allMovieIdsFromSearch.add(searchResults[i].getInt("id"));
+            }
+
+            List<JSONObject> allMoviesFromSearch = getAllMoviesByIds(allMovieIdsFromSearch);
+
+            TextView textView = findViewById(R.id.textView2);
+            textView.setText(allMoviesFromSearch.toString());
+
+        } catch (JSONException e) {
+            String error = "Something went wrong, sorry.";
+            e.printStackTrace();
+        }
+    }
+
+    private List<JSONObject> getAllMoviesByIds(List<Integer> ids){
+        List<JSONObject> allMovies = new ArrayList<>();
+        for (Integer id:ids) {
+            JSONObject movieJSON = jsonReader.getJson("https://api.themoviedb.org/3/movie/"
+                    + id
+                    + "?api_key="
+                    + APIKey
+                    + "&language=en-US"
+            );
+            allMovies.add(movieJSON);
+
+        }
+        return allMovies;
+    }
+}
